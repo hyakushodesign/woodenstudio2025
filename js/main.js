fc934e629daaa70f50d23c256de66226179a2554
hyakushodesign <165166740+hyakushodesign@users.noreply.github.com>
2025-06-26 22:11:42 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,3 +15,63 @@ gsap.fromTo(
     }
     }
 )
+
+gsap.fromTo(
+    ".concept-first .column-right",
+    2,
+    {
+        x: 100,
+        opacity: 0
+    },
+    {
+        x: 0,
+    opacity: 1,
+    delay: 0.5,
+    scrollTrigger:{
+        trigger: ".concept-first .column-right",
+        start: "top center",
+        // markers: true
+    }
+    }
+)
+
+
+
+
+
+gsap.fromTo(
+    ".concept-second .column-left",
+    2,
+    {
+        x: -100,
+        opacity: 0
+    },
+    {
+        x: 0,
+    opacity: 1,
+    delay: 0.5,
+    scrollTrigger:{
+        trigger: ".concept-second .column-left",
+        start: "top center",
+        // markers: true
+    }
+    }
+)
+
+gsap.fromTo(
+    ".concept-second .column-right",
+    2,
+    {
+        x: 100,
+        opacity: 0
+    },
+    {
+        x: 0,
+    opacity: 1,
+    scrollTrigger:{
+        trigger: ".concept-second .column-right",
+        start: "top center",
+        // markers: true
+    }
+    }
+)
